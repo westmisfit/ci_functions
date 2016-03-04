4cc23ffa38fe02d5b6b20eda5f876e574b1372b3
West Wang <west@misfit.com>
2016-03-04 17:00:09 +0800

    wirte new format docker config file

--- functions.sh
+++ functions.sh
@@ -2,8 +2,33 @@
 # functions library, don't run this file directly
 # auth: west@misfit.com
 # created: 2015-11-02
-# modified:
+# modified: 2016-03-04
 ##
+
+function dockerLoginHelp
+{
+  echo "Usage: dockerLogin --email {email} --auth {auth}}" 1>&2
+}
+
+function dockerLogin
+{
+  local email=""
+  local auth=""
+
+  while [[ $# > 0 ]]
+  do
+    key="$1"
+    case $key in
+      --auth) auth="$2"; shift;;
+      --email) email="$2"; shift;;
+      *) dockerLoginHelp; return 1;;
+    esac
+    shift
+  done
+
+  echo '{"https://index.docker.io/v1/":{"auth":"'$auth'","email":"'$email'"}}' > ~/.dockercfg
+  echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'$auth'","email":"'$email'"}}}' > ~/.docker/config.json
+}
 
 function loadDockerImages
 {
