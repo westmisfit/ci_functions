6c187b1c1fe2d9b8b139473774e6681ee11164b9
West Wang <west@misfit.com>
2016-03-04 17:30:55 +0800

    fix ~/.docker/ dir not exist error

--- functions.sh
+++ functions.sh
@@ -26,7 +26,11 @@ function dockerLogin
     shift
   done
 
+  # generate old version docker config file
   echo '{"https://index.docker.io/v1/":{"auth":"'$auth'","email":"'$email'"}}' > ~/.dockercfg
+
+  # generate new version docker config file
+  mkdir ~/.docker/
   echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'$auth'","email":"'$email'"}}}' > ~/.docker/config.json
 }
 
